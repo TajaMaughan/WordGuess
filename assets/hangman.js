5ba8debbf7c718aa9f36c14001412fe5215e8992
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2018-11-08 12:02:12 -0700

    working on getting the spaces to be replaced by the letters without replacing all of the spaces.

--- assets/hangman.js
+++ assets/hangman.js
@@ -10,10 +10,11 @@
     //create a library of words for the computer to pick from
     var wordPool = ["tiger", "lion", "baboon", "gorilla", "orangutang", "giraffe", "zebra", "anaconda", "tarantula", "rhinoceros", "leopard", "panda"];
 
-    //have computer randomly pick a word for user to guess
+    //have computer randomly  pick a word for user to guess
     function randomWord() {
         word = wordPool[Math.floor(Math.random() * wordPool.length)];
         console.log(word);
+        return word;
     };
     //display number of spaces for chosen word
     function numberOfLetters() {
@@ -21,34 +22,39 @@
             for (var i = 0; i < word.length; i++) {
         wordSpaces = ["_ "];
         $("#letters").append(wordSpaces);
-        }   
+        }
+        return wordSpaces;
     };
     //word.split
     function letterInWord() {
         letters = word.split("");
         console.log(letters);
+        return letters;
     };
-    //create a loop that will allow the user to guess X number of times
-
-    for (var guesses; guesses = 0; guesses--) {
-     //after each guess decrease j by 1;
-    }
-
     //Have the user make a guess
+        //In start function.
+
+
+
+
+
+
+
+function gameStart() {
+    randomWord();
+    numberOfLetters();
+    letterInWord();
     document.onkeyup = function theGuess(event) {
-        var userGuess = event.key;
-   
+        var userGuess = event.key;   
         for (j = 0; j < letters.length; j++) {
             if (userGuess === letters[j]) {
-                $("#letters").html(letters[j]);
+                wordSpaces[j] = userGuess;
+                $("#letters").html(wordSpaces)
             }
         }
+        return userGuess;
     }
-    function gameStart() {
-        randomWord();
-        numberOfLetters();
-        letterInWord();
-    };
+}
 
     // check the users guess against letters in the word
 
@@ -57,6 +63,6 @@
 
     /*If guess is incorrect, let the user know to try again., 
     and log incorrect letter to the DOM so the user knows what they have already guessed*/
-
+    //create a loop that will allow the user to guess incorrectly X number of times
     //once the whole word is guessed, or the number of guess run out, restart the game with a new word.
 

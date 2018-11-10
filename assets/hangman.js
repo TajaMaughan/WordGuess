4d122326bdd87b265b444414f7e342d408aa414b
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2018-11-09 23:27:55 -0700

    got wins, loses, and remaining guesses working

--- assets/hangman.js
+++ assets/hangman.js
@@ -1,65 +1,70 @@
 
 //USE FUNCTIONS!!
-    var wins = 0;
-    var loses = 0;
-    var word = "";
-    var wordSpaces = [];
-    var letters = [];
-    var guessTotal = 10;
-    var userGuess;
-    var alreadyGuessed;
+var wins = 0;
+var loses = 0;
+var word = "";
+var wordSpaces = [];
+var letters = [];
+var guessTotal = 10;
+var userGuess;
+var alreadyGuessed;
         
-    //create a library of words for the computer to pick from
-    var wordPool = ["tiger", "lion", "baboon", "gorilla", "orangutang", "giraffe", "zebra", "anaconda", "tarantula", "rhinoceros", "leopard", "panda"];
+//create a library of words for the computer to pick from
+var wordPool = ["tiger", "lion", "baboon", "gorilla", "orangutang", "giraffe", "zebra", "anaconda", "tarantula", "rhinoceros", "leopard", "panda"];
 
-    //have computer randomly  pick a word for user to guess
-    function randomWord() {
-        word = wordPool[Math.floor(Math.random() * wordPool.length)];
-        console.log(word);
-        return word;
-    };
-    //display number of spaces for chosen word
-    function numberOfLetters() {
-        for (var i = 0; i < word.length; i++) {
-            wordSpaces.push("_");
-            $("#letters").html(wordSpaces);
-        }
-        console.log(wordSpaces)
-        return wordSpaces;
-    };
-    //word.split
-    function letterInWord() {
-        letters = word.split([""]);
-        console.log(letters);
-        return letters;
-    };
-    //Have the user make a guess
-        //In start function.
-
+//have computer randomly  pick a word for user to guess
+function randomWord() {
+    word = wordPool[Math.floor(Math.random() * wordPool.length)];
+    console.log(word);
+    return word;
+};
+//display number of spaces for chosen word
+function numberOfLetters() {
+    for (var i = 0; i < word.length; i++) {
+        wordSpaces.push("_");
+        $("#letters").html(wordSpaces);
+    }
+    console.log(wordSpaces)
+    return wordSpaces;
+};
+//word.split
+function letterInWord() {
+    letters = word.split([""]);
+    console.log(letters);
+    return letters;
+};
 
 $(document).ready(function() {
     randomWord();
     numberOfLetters();
     letterInWord();
     $("#totalGuesses").html(guessTotal);
+    //Have the user make a guess    
     document.onkeyup = function theGuess(event) {
         var userGuess = event.key;
+/*If guess is incorrect, log incorrect letter to the DOM 
+so the user knows what they have already guessed*/        
         if (letters.indexOf(userGuess) === -1) {
             $("#guessed").append(userGuess);
+//create a loop that will allow the user to guess incorrectly X number of times            
             guessTotal--;
             $("#totalGuesses").html(guessTotal);
         } else {
+// check the users guess against letters in the word            
             for (i = 0; i < letters.length; i++) {
                 if (userGuess === letters[i]) {              
                     wordSpaces[i] = userGuess;
+//If guess is correct have it write to the DOM                    
                     $("#letters").html(wordSpaces)
                 }
             }
         }
+//once total guesses run out, log a loss.        
         if ( guessTotal === 0) {
             loses++;
             $("#loses").html(loses);
         }
+//if word is guessed, log a win.        
         if (wordSpaces.indexOf("_") === -1) {
             wins++;
             $("#wins").html(wins);
@@ -68,13 +73,13 @@ $(document).ready(function() {
     }
 })
                 
-    // check the users guess against letters in the word
 
 
-    //If guess is correct have it write to the DOM
 
-    /*If guess is incorrect, let the user know to try again., 
-    and log incorrect letter to the DOM so the user knows what they have already guessed*/
-    //create a loop that will allow the user to guess incorrectly X number of times
-    //once the whole word is guessed, or the number of guess run out, restart the game with a new word.
+
+
+
+
+
+
 

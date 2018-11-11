09898b3e6cfec5593fbb0f23fea8f4462f2b513f
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2018-11-10 18:30:37 -0700

    finished game

--- assets/hangman.js
+++ assets/hangman.js
@@ -5,9 +5,10 @@ var wordSpaces = [];
 var letters = [];
 var guessTotal = 10;
 var userGuess;
-var alreadyGuessed= [];
+var alreadyGuessed = [];
+var guessedWords = [];
 //create a library of words for the computer to pick from
-var wordPool = ["tiger", "lion", "baboon", "gorilla", "orangutang", "giraffe", "zebra", "anaconda", "tarantula", "rhinoceros", "leopard", "panda"];
+var wordPool = ["tiger", "lion", "baboon", "gorilla", "orangutang", "giraffe", "zebra", "anaconda", "tarantula", "rhinoceros", "leopard", "panda", "ocelot", "moose", "hippopotamus", "camel", "llama", "otter", "scorpion", "cougar", "python", "walrus", "penguin", "octopus", "elephant", "sloth", "monkey"];
 //have computer randomly  pick a word for user to guess
 function randomWord() {
     word = wordPool[Math.floor(Math.random() * wordPool.length)];
@@ -28,9 +29,20 @@ function letterInWord() {
     letters = word.split([""]);
     console.log(letters);
     return letters;
-};
+}
+//reset
+function reset() {
+    alreadyGuessed = [];
+    wordSpaces = [];
+    $("#guessed").html("Wrong guesses: ");
+    guessTotal = 10;
+    $("#totalGuesses").html(guessTotal);
+    randomWord();
+    numberOfLetters();
+    letterInWord();
+}
 
-$(document).ready(function() {
+$(document).ready(function () {
     randomWord();
     numberOfLetters();
     letterInWord();
@@ -38,40 +50,45 @@ $(document).ready(function() {
     //Have the user make a guess    
     document.onkeyup = function theGuess(event) {
         var userGuess = event.key;
-/*If guess is incorrect, log incorrect letter to the DOM 
-so the user knows what they have already guessed*/        
+        /*If guess is incorrect, log incorrect letter to the DOM 
+        so the user knows what they have already guessed*/
         if (letters.indexOf(userGuess) === -1) {
-            if (alreadyGuessed.indexOf(userGuess) === -1){           
-            $("#guessed").append(userGuess);
-            alreadyGuessed.push(userGuess);
-//create a loop that will allow the user to guess incorrectly X number of times            
-            guessTotal--;
-            $("#totalGuesses").html(guessTotal);
+            if (alreadyGuessed.indexOf(userGuess) === -1) {
+                $("#guessed").append(" " + userGuess);
+                alreadyGuessed.push(userGuess);
+                //create a loop that will allow the user to guess incorrectly X number of times            
+                guessTotal--;
+                $("#totalGuesses").html(" " + guessTotal);
             }
         } else {
-// check the users guess against letters in the word            
+            // check the users guess against letters in the word            
             for (i = 0; i < letters.length; i++) {
-                if (userGuess === letters[i]) {              
+                if (userGuess === letters[i]) {
                     wordSpaces[i] = userGuess;
-//If guess is correct have it write to the DOM                    
+                    //If guess is correct have it write to the DOM                    
                     $("#letters").html(wordSpaces)
                 }
             }
         }
-//once total guesses run out, log a loss.        
-        if ( guessTotal === 0) {
+        //once total guesses run out, log a loss.        
+        if (guessTotal === 0) {
             loses++;
-            $("#loses").html(loses);
+            $("#loses").html("  " + loses);
+            $("#result").html("You lose.")
+            reset();
         }
-//if word is guessed, log a win.        
+        //if word is guessed, log a win.        
         if (wordSpaces.indexOf("_") === -1) {
             wins++;
-            $("#wins").html(wins);
+            $("#wins").html("  " + wins);
+            $("#result").html("You win!")
+            $("#words").append(" " + word + ",");
+            reset();
         }
 
     }
 })
-                
+
 
 
 
